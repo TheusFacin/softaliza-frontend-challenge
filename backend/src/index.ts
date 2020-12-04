f6fe19b7dfd9aa7913da1af3c163b583218d9520
Matheus Facin <matheus.facin@outlook.com>
2020-12-04 20:28:20 -0300

    Backend - Configure cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import router from './routes'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -20,8 +21,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 const PORT = process.env.PORT || 3333
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
 app.use(router)
 
 app.listen(PORT, () => console.log(`> Server listen on port ${PORT}`))
