35d682259e43e022dc190efda5fb95461d4d89e0
Matheus Facin <matheus.facin@outlook.com>
2020-12-05 23:14:05 -0300

    Frontend - Create input validation

--- frontend/src/components/EventForm/index.tsx
+++ frontend/src/components/EventForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { EventTypeEnum } from '../../types'
 
 import Input, { TextArea } from '../Input'
@@ -6,25 +6,124 @@ import Input, { TextArea } from '../Input'
 import './styles.scss'
 
 const EventForm: React.FC = () => {
-  const [eventType, setEventType] = useState<EventTypeEnum>()
+  const [eventType, setEventType] = useState<EventTypeEnum>(
+    EventTypeEnum.ONLINE
+  )
+
+  const formRef = useRef<HTMLFormElement>(null)
+  const titleRef = useRef<HTMLInputElement>(null)
+  const dateRef = useRef<HTMLInputElement>(null)
+  const hourRef = useRef<HTMLInputElement>(null)
+  const emailRef = useRef<HTMLInputElement>(null)
+  const phoneRef = useRef<HTMLInputElement>(null)
+  const descriptionRef = useRef<HTMLTextAreaElement>(null)
+  const urlRef = useRef<HTMLInputElement>(null)
+  const addressRef = useRef<HTMLInputElement>(null)
+
+  const handleVerifyInputs = (): boolean => {
+    let hasErrors = false
+
+    if (!titleRef.current?.value) {
+      titleRef.current?.setCustomValidity('Insira um título')
+      hasErrors = true
+    }
+
+    if (!dateRef.current?.value) {
+      dateRef.current?.setCustomValidity('Insira uma data')
+      hasErrors = true
+    }
+
+    if (!hourRef.current?.value) {
+      hourRef.current?.setCustomValidity('Insira um horário')
+      hasErrors = true
+    }
+
+    if (!emailRef.current?.value) {
+      emailRef.current?.setCustomValidity('Insira um email')
+      hasErrors = true
+    }
+
+    if (!phoneRef.current?.value) {
+      phoneRef.current?.setCustomValidity('Insira um telefone')
+      hasErrors = true
+    }
+
+    if (!descriptionRef.current?.value) {
+      descriptionRef.current?.setCustomValidity('Insira uma descrição')
+      hasErrors = true
+    }
+
+    if (
+      (eventType === EventTypeEnum.HYBRID ||
+        eventType === EventTypeEnum.ONLINE) &&
+      !urlRef.current?.value
+    ) {
+      urlRef.current?.setCustomValidity('Insira uma URL')
+      hasErrors = true
+    }
+
+    if (
+      (eventType === EventTypeEnum.HYBRID ||
+        eventType === EventTypeEnum.PRESENTIAL) &&
+      !addressRef.current?.value
+    ) {
+      addressRef.current?.setCustomValidity('Insira uma URL')
+      hasErrors = true
+    }
+
+    return hasErrors
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!handleVerifyInputs()) return
+  }
 
   return (
     <div className="form-container">
-      <form>
+      <form ref={formRef} onSubmit={handleSubmit}>
         <div className="row">
-          <Input label="Título" placeholder="Digite o nome do evento" mask="" />
+          <Input
+            label="Título"
+            placeholder="Digite o nome do evento"
+            mask=""
+            maxLength={50}
+            onKeyUp={e =>
+              e.currentTarget.value.length < 5
+                ? e.currentTarget.setCustomValidity('Insira um título maior')
+                : e.currentTarget.setCustomValidity('')
+            }
+            ref={titleRef}
+          />
           <div className="row">
             <Input
               label="Data"
               placeholder="Informe a data"
               mask="99/99/9999"
               inputMode="numeric"
+              onKeyUp={e =>
+                !e.currentTarget.value ||
+                !/[0-3]\d\/[0-1]\d\/\d\d\d\d/.test(e.currentTarget.value)
+                  ? e.currentTarget.setCustomValidity('Insira uma data válida')
+                  : e.currentTarget.setCustomValidity('')
+              }
+              ref={dateRef}
             />
             <Input
               label="Horário"
               placeholder="Informe o horário"
               mask="99:99"
               inputMode="numeric"
+              onKeyUp={e =>
+                !e.currentTarget.value ||
+                !/[01]\d:[0-5]\d/.test(e.currentTarget.value)
+                  ? e.currentTarget.setCustomValidity(
+                      'Insira um horário válido'
+                    )
+                  : e.currentTarget.setCustomValidity('')
+              }
+              ref={hourRef}
             />
           </div>
         </div>
@@ -36,6 +135,7 @@ const EventForm: React.FC = () => {
             inputMode="email"
             placeholder="Entre com um email para contato"
             mask=""
+            ref={emailRef}
           />
           <Input
             label="Telefone"
@@ -43,10 +143,27 @@ const EventForm: React.FC = () => {
             inputMode="tel"
             placeholder="Entre com um telefone para contato"
             mask="(99) 9999-9999"
+            onKeyUp={e =>
+              !e.currentTarget.value ||
+              !/\(\d\d\) \d\d\d\d-\d\d\d\d/.test(e.currentTarget.value)
+                ? e.currentTarget.setCustomValidity('Insira um telefone válido')
+                : e.currentTarget.setCustomValidity('')
+            }
+            ref={phoneRef}
           />
         </div>
 
-        <TextArea label="Descrição" placeholder="Descreva o seu evento" />
+        <TextArea
+          label="Descrição"
+          span="(Mínimo 20 caracteres)"
+          placeholder="Descreva o seu evento"
+          ref={descriptionRef}
+          onKeyUp={e =>
+            e.currentTarget.value.length < 20
+              ? e.currentTarget.setCustomValidity('Insira uma descrição maior')
+              : e.currentTarget.setCustomValidity('')
+          }
+        />
 
         <hr />
 
@@ -90,10 +207,12 @@ const EventForm: React.FC = () => {
           eventType === EventTypeEnum.HYBRID) && (
           <Input
             label="URL do evento"
+            span="(Informar http:// ou https://)"
             placeholder="Informe o site que o evento ocorrerá"
             mask=""
             type="url"
             inputMode="url"
+            ref={urlRef}
           />
         )}
 
@@ -101,8 +220,18 @@ const EventForm: React.FC = () => {
           eventType === EventTypeEnum.HYBRID) && (
           <Input
             label="Endereço"
+            span={'(No formato "endereço - cidade - estado")'}
             placeholder="Informe o local do evento"
             mask=""
+            onKeyUp={e =>
+              !e.currentTarget.value ||
+              !/.+ - .+ - .+/.test(e.currentTarget.value)
+                ? e.currentTarget.setCustomValidity(
+                    'Insira um endereço comforme o padrão descrito'
+                  )
+                : e.currentTarget.setCustomValidity('')
+            }
+            ref={addressRef}
           />
         )}
 
--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react'
+import React, { forwardRef, InputHTMLAttributes } from 'react'
 import InputMask from 'react-input-mask'
 
 import './styles.scss'
@@ -7,7 +7,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
   span?: string
   placeholder: string
-  mask: string
+  mask: string | (string | RegExp)[]
 }
 
 interface TextareaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
@@ -16,40 +16,35 @@ interface TextareaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
   placeholder: string
 }
 
-const Input: React.FC<InputProps> = ({
-  label,
-  span,
-  placeholder,
-  mask,
-  ...props
-}) => {
-  return (
-    <div className="input-container">
-      <label>
-        {label}
-        {span && <span>{span}</span>}
-      </label>
-      <InputMask mask={mask} placeholder={placeholder} {...props} />
-    </div>
-  )
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, span, placeholder, mask, ...props }, ref) => {
+    return (
+      <div className="input-container">
+        <label>
+          {label}
+          {span && <span>{span}</span>}
+        </label>
+        <InputMask mask={mask}>
+          {() => <input placeholder={placeholder} ref={ref} {...props} />}
+        </InputMask>
+      </div>
+    )
+  }
+)
 
-const TextArea: React.FC<TextareaProps> = ({
-  label,
-  span,
-  placeholder,
-  ...props
-}) => {
-  return (
-    <div className="input-container">
-      <label>
-        {label}
-        {span && <span>{span}</span>}
-      </label>
-      <textarea placeholder={placeholder} {...props}></textarea>
-    </div>
-  )
-}
+const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ label, span, placeholder, ...props }, ref) => {
+    return (
+      <div className="input-container">
+        <label>
+          {label}
+          {span && <span>{span}</span>}
+        </label>
+        <textarea placeholder={placeholder} ref={ref} {...props}></textarea>
+      </div>
+    )
+  }
+)
 
 export default Input
 export { TextArea }
